1bce1acaf59f361fe5ea1b5c771a02565bfaf860
Mangocod3r <117846037+Mangocod3r@users.noreply.github.com>
2023-01-31 18:39:49 +0530

    Update Stu_ideas.js

--- src/pages/Stu_ideas.js
+++ src/pages/Stu_ideas.js
@@ -1,6 +1,10 @@
-export function Stu_ideas() {
+import Header from '../components/header_s';
+import Footer from '../components/footer'
+
+export function Stuideas() {
     return (
             <>
+            <Header></Header>
                 <div className="container-fluid main" id="productTable">
         <table className="table-fill table-bordered">
           <thead>
@@ -22,7 +26,7 @@ export function Stu_ideas() {
                       A project in which you can process images of an entire area or region by tracking people on the road or streets to analyze if they are wearing masks or not 
                     </p>
                     <div className="display-5 text-center">
-                      <a href="stu_view_idea.html">
+                      <a href="stu_view_idea">
                         <button type="button" className="btn btn-block btn-success">
                           SEE YOUR IDEA
                         </button>
@@ -43,7 +47,7 @@ export function Stu_ideas() {
                     <p style={{fontSize: '20px'}}>
                       One of the best projects to work with alphanumeric character identification is with the help of number plate images.
                     </p><div className="display-5 text-center">
-                      <a href="stu_view_idea.html">
+                      <a href="stu_view_idea">
                         <button type="button" className="btn btn-block btn-success">
                           SEE YOUR IDEA
                         </button>
@@ -64,7 +68,7 @@ export function Stu_ideas() {
                     <p style={{fontSize: '20px'}}>
                       One of the most significant contributions of image processing, computer vision, machine learning, and deep learning is in the medical field. They contribute to analyzing and visualizing many of the highly complex abnormalities that could occur in human beings.
                     </p><div className="display-5 text-center">
-                      <a href="stu_view_idea.html">
+                      <a href="stu_view_idea">
                         <button type="button" className="btn btn-block btn-success">
                           SEE YOUR IDEA
                         </button>
@@ -85,7 +89,7 @@ export function Stu_ideas() {
                     <p style={{fontSize: '20px'}}>
                       You might want to figure out the emotions on one face. Whether the person shows signs of happiness, sadness, anger, or any other similar emotion, you can build an AI model that will perform the following classification.
                     </p><div className="display-5 text-center">
-                      <a href="stu_view_idea.html">
+                      <a href="stu_view_idea">
                         <button type="button" className="btn btn-block btn-success">
                           SEE YOUR IDEA
                         </button>
@@ -106,7 +110,7 @@ export function Stu_ideas() {
                     <p style={{fontSize: '20px'}}>
                       With the introduction of online classes where students and teachers interact through an online platform, it would be harder to take attendance in the more traditional way. However, computer vision comes to the rescue to help us create an image-based attendance system for taking attendance online with the help of your pixelated pictures!
                     </p><div className="display-5 text-center">
-                      <a href="stu_view_idea.html">
+                      <a href="stu_view_idea">
                         <button type="button" className="btn btn-block btn-success">
                           SEE YOUR IDEA
                         </button>
@@ -119,6 +123,7 @@ export function Stu_ideas() {
           </tbody>
         </table>
       </div>
+      <Footer></Footer>
             </>
            )
 }
